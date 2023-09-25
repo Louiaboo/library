0e0fb03ca8a664d3db77fe36174096d2c994966f
Louiaboo <106045306+Louiaboo@users.noreply.github.com>
2023-09-25 13:10:53 +0800

    Fix modal layout

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const modal = document.querySelector(".modal");
 const newEntry = document.querySelector(".new");
+const body = document.querySelector("body");
 
 newEntry.addEventListener("click", () => {
-  modal.style.display = "block";
+  modal.style.display = "flex";
 });
 
 // Not working idk why
